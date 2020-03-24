3cc8658b5f2c20094eb7a0abd388c5b809b84d0c
SE18302010003 <07f40e26798025351f97c00a83b1ce99@huawei.com>
2020-03-24 20:12:21 +0800

    端口修改为8080

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsPublicPath: './',
     proxyTable: {
       '/api': {
-        target: 'http://114.115.236.28:80',
+        target: 'http://114.115.236.28:8080',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
@@ -21,7 +21,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: '114.115.236.28', // can be overwritten by process.env.HOST
-    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
