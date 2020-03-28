e662df23e6df0a4bc4c6a45cdf6eee4056edcabb
Honminden <Honminden@outlook.com>
2020-03-29 06:22:11 +0800

    added bootstrap framework; removed element-ui package

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,16 +11,5 @@ export default {
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    /*margin-top: 60px;*/
-  }
-  body{
-    margin: 0px;
-    padding: 0px;
-  }
+  @import url('./style/bootstrap.min.css');
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import {router} from './router'
 import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.use(ElementUI)
+require('jquery')
+require('popper.js')
+require('bootstrap')
 
 //axios 配置
 var axios = require('axios')
