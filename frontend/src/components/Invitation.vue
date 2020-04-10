153d3f8193ec98d7fd452dd5ffc4554348754ac5
Shi Anqi <Honminden@outlook.com>
2020-04-10 19:23:21 +0800

    fixed get invitation

--- frontend/src/components/Invitation.vue
+++ frontend/src/components/Invitation.vue
@@ -5,46 +5,82 @@
     <div v-show="alert.isVisible" :class="alert.type">
       {{ alert.content }}
     </div>
-    <div class="row mt-4">
-      <div class="col-2"></div>
-      <div class="col-8">
-        <form class="form-inline" style="margin-top:25px">
-          <legend>Invite PC Members</legend>
-          <input class="form-control" type="search" placeholder="Search User..." v-model="userFullName">
-          <button class="btn btn-outline-primary ml-2" @click.prevent="getList()">Search</button>
-        </form>
-
-        <table class="table table-hover form-group" style="margin-top: 30px">
-          <tbody>
-            <tr class="bg-light">
-              <th scope="col">
-              </th>
-              <th scope="col">Username</th>
-              <th scope="col">Full Name</th>
-              <th scope="col">Email</th>
-              <th scope="col">Organization</th>
-              <th scope="col">Region</th>
-            </tr>
-            <tr v-for="user in userList" :key="user.username">
-              <td>
-                <div class="custom-control custom-checkbox">
-                  <input type="checkbox" class="custom-control-input" :id="'U' + user.username" v-model="user.checked">
-                  <label class="custom-control-label" :for="'U' + user.username"></label>
-                </div>
-              </td>
-              <td>{{ user.username }}</td>
-              <td>{{ user.fullName }}</td>
-              <td>{{ user.email }}</td>
-              <td>{{ user.organization }}</td>
-              <td>{{ user.region }}</td>
-            </tr>
-          </tbody>
-        </table>
-        <div>
-          <button class="btn btn-outline-primary float-right" type="button" style="margin-right: 50px">Invite</button>
+    <div class="accordion" id="accordion">
+      <div class="card">
+        <button class="btn btn-primary" data-toggle="collapse" data-target="#invitations">
+          Previous Invitations
+        </button>
+        <div id="invitations" class="collapse show" data-parent="#accordion">
+          <div class="card-body">
+            <table class="table table-hover form-group" style="margin-top: 30px">
+              <tbody>
+                <tr class="bg-light">
+                  <th scope="col">Receiver</th>
+                  <th scope="col">Status</th>
+                </tr>
+                <tr v-for="invitation in invitationList" :key="invitation.receiver">
+                  <td>{{ invitation.receiver }}</td>
+                  <td>
+                    <span v-if="invitation.status === 'waiting'" class="badge badge-warning">waiting</span>
+                    <span v-else-if="invitation.status === 'passed'" class="badge badge-info">passed</span>
+                    <span v-else-if="invitation.status === 'rejected'" class="badge badge-danger">rejected</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-      <div class="col-2"></div>
+      <div class="card">
+        <button class="btn btn-primary" data-toggle="collapse" data-target="#invite">
+          Send New Invitations
+        </button>
+        <div id="invite" class="collapse" data-parent="#accordion">
+          <div class="card-body row mt-4">
+            <div class="col-2"></div>
+            <div class="col-8">
+              <form class="form-inline" style="margin-top:25px">
+                <legend>Invite PC Members</legend>
+                <input class="form-control" type="search" placeholder="Search User..." v-model="userFullName">
+                <button class="btn btn-outline-primary ml-2" @click.prevent="getUsers()">Search</button>
+              </form>
+
+              <table class="table table-hover form-group" style="margin-top: 30px">
+                <tbody>
+                  <tr class="bg-light">
+                    <th scope="col">
+                    </th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Full Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Organization</th>
+                    <th scope="col">Region</th>
+                  </tr>
+                  <tr v-for="user in userList" :key="user.username">
+                    <td>
+                      <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" :id="'U' + user.username" v-model="user.checked">
+                        <label class="custom-control-label" :for="'U' + user.username"></label>
+                      </div>
+                    </td>
+                    <td>{{ user.username }}</td>
+                    <td>{{ user.fullName }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>{{ user.organization }}</td>
+                    <td>{{ user.region }}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div>
+                <button class="btn btn-outline-info float-right mr-2" type="button" @click="postInvitations()">
+                  Invite
+                  </button>
+              </div>
+            </div>
+            <div class="col-2"></div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -62,7 +98,13 @@ export default {
       user: new User(),
       alert: new Alert(),
       userFullName: '',
-      userList: []
+      userList: [],
+      invitationList: [],
+      alertCounter: {
+        total: 0,
+        successCount: 0,
+        failureCount: 0
+      }
     }
   },
   props: ['username', 'fullName', 'abbreviation', 'time', 'location', 'submissionDDL', 'reviewReleaseDate', 'status', 'role'],
@@ -72,11 +114,11 @@ export default {
     'InnerNav': InnerNav
   },
   methods: {
-    getList()
+    getUsers()
     {
       this.$axios.get('/user', {
         params: {
-          conference: this.conference,
+          conference: this.fullName,
           inviter: this.user.getUserInfo().username,
           fullName: this.userFullName
         }
@@ -84,7 +126,7 @@ export default {
       .catch(
         error =>
         {
-          //this.alert.popDanger('fetch list error');
+          this.alert.popDanger('fetch users error');
         }
       )
       .then(res =>
@@ -95,10 +137,81 @@ export default {
           this.userList.forEach(user => {
             user.checked = false;
           });
-          console.log(this.userList);
         }
       });
+    },
+    getInvitations () {
+      this.$axios.get('/invitation', {
+        params: {
+          inviter: this.user.getUserInfo().username,
+          receiver: ''
+        }
+      })
+      .catch(
+        error =>
+        {
+          this.alert.popDanger('fetch invitations error');
+        }
+      )
+      .then(res =>
+      {
+        if(res && res.status === 200)
+        {
+          this.invitationList = res.data.invitationList;
+        }
+      });
+    },
+    postInvitations () {
+      let receivers = this.userList.filter(user => (user.checked))
+      this.alertCounter.total = receivers.length;
+      for (let receiver of receivers)
+      {
+        this.$axios.post('/invitation', {
+            conference: this.fullName,
+            inviter: this.user.getUserInfo().username,
+            receiver: receiver.username
+          })
+          .catch(
+            error =>
+            {
+              this.alertCounter.failureCount++;
+              this.updateAlert();
+            }
+          )
+          .then(res =>
+          {
+            if(res && res.status === 200)
+            {
+              this.alertCounter.successCount++;
+              this.updateAlert();
+            }
+          });
+      }
+    },
+    updateAlert () {
+      let total = this.alertCounter.total;
+      let successCount = this.alertCounter.successCount;
+      let failureCount = this.alertCounter.failureCount;
+      if ((total != 0) && ((successCount + failureCount) === total))
+      {
+        if (failureCount > 0)
+        {
+          this.alert.popDanger(`${successCount} invitations sent, ${failureCount} invitations failed`);
+        }
+        else
+        {
+          this.alert.popSuccess(`${successCount} invitations sended`);
+          setTimeout(() => 
+          {
+            this.$router.go();
+          }, 1500);
+        }
+      }
     }
+  },
+  mounted () {
+    document.title += ` - ${this.fullName}`;
+    this.getInvitations();
   }
 }
 
