7e5f8662841d859cf9f3da4fdf15e24a3cd4e477
Shi Anqi <Honminden@outlook.com>
2020-04-20 23:49:30 +0800

    fixed admin accessibility and improved appearance of left nav

--- frontend/src/components/LeftNav.vue
+++ frontend/src/components/LeftNav.vue
@@ -1,21 +1,21 @@
 <template>
-  <ul class="nav nav-pills navbar-expand-sm flex-column bg-light navbar-light text-left" style="min-height: 100vh">
+  <ul class="nav nav-pills navbar-expand-sm flex-column bg-light navbar-light text-left" style="min-height: 100vh; min-width: 15vw;">
     <li class="nav-item ml-2">
       <router-link class="nav-link m-2 rounded" :class="[{ 'active': highlight === 'userInfo' }]" to="userInfo">
-        <i class="fa fa-id-card mx-2"></i>UserInfo
+        <i class="fa fa-id-card mx-2"></i>User Info
       </router-link>
     </li>
     <li class="nav-item ml-2">
       <router-link class="nav-link m-2 rounded" :class="[{ 'active': highlight === 'list' }]" to="list">
-        <i class="fa fa-th-list mx-2"></i>List
+        <i class="fa fa-th-list mx-2"></i>List of Conferences
       </router-link>
     </li>
-    <li class="nav-item ml-2">
+    <li v-if="!user.isAdmin()" class="nav-item ml-2">
       <router-link class="nav-link m-2 rounded" :class="[{ 'active': highlight === 'conference' }]" to="conference">
-        <i class="fa fa-file-o mx-2"></i>Application
+        <i class="fa fa-file-o mx-2"></i>New Conference
       </router-link>
     </li>
-    <li class="nav-item ml-2">
+    <li v-if="!user.isAdmin()" class="nav-item ml-2">
       <router-link class="nav-link m-2 rounded"  :class="[{ 'active': highlight === 'notification' }]" to="notification">
         <i class="fa fa-envelope mx-2"></i>Notifications
       </router-link>
@@ -24,11 +24,13 @@
 </template>
 
 <script>
+import User from './User/User'
 
 export default {
   name: "LeftNav",
   data(){
     return {
+      user: new User(),
       highlight: ""
     }
   },
