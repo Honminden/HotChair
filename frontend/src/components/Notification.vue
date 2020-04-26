8307643c0928315ffbab4aaa775ba3a20f84d1ff
Shi Anqi <Honminden@outlook.com>
2020-04-26 13:02:29 +0800

    added topic frame for notification

--- frontend/src/components/Notification.vue
+++ frontend/src/components/Notification.vue
@@ -16,6 +16,7 @@
                     <tr>
                       <th scope="col">Conference</th>
                       <th scope="col">Chair</th>
+                      <th scope="col">Topics</th>
                       <th scope="col"></th>
                     </tr>
                     <tr v-for="invitation in invitationList.filter(invitation => (invitation.status === 'waiting'))"
@@ -23,11 +24,14 @@
                       <td>{{ invitation.conference }}</td>
                       <td>{{ invitation.inviter }}</td>
                       <td>
-                        <button class="btn btn-outline-success"
-                                  @click="putStatus(invitation.conference, invitation.inviter, 'accepted')">
+                        <span class="badge badge-primary mx-2">topicA</span>
+                        <span class="badge badge-primary mx-2">topicB</span>
+                      </td>
+                      <td>
+                        <button class="btn btn-outline-success" data-toggle="modal" data-target="#topic">
                           Accept
                         </button>
-                        <button class="btn btn-outline-danger ml-2"
+                        <button class="btn btn-outline-danger ml-2" 
                                   @click="putStatus(invitation.conference, invitation.inviter, 'rejected')">
                           Reject
                         </button>
@@ -49,12 +53,17 @@
                     <tr>
                       <th scope="col">Conference</th>
                       <th scope="col">Chair</th>
+                      <th scope="col">Topics</th>
                       <th scope="col">Status</th>
                     </tr>
                     <tr v-for="invitation in invitationList.filter(invitation => (invitation.status !== 'waiting'))"
                               :key="invitation.inviter">
                       <td>{{ invitation.conference }}</td>
                       <td>{{ invitation.inviter }}</td>
+                      <td>
+                        <span class="badge badge-primary mx-2">topicA</span>
+                        <span class="badge badge-primary mx-2">topicB</span>
+                      </td>
                       <td>
                         <span v-if="invitation.status === 'accepted'" class="badge badge-success">accepted</span>
                         <span v-else-if="invitation.status === 'rejected'" class="badge badge-danger">rejected</span>
@@ -69,6 +78,29 @@
         <div>
           <div class="col-sm-3 float-right text-center" v-show="alert.isVisible" :class="alert.type">
             <i :class="alert.icon"></i>{{ alert.content }}
+          </div>
+        </div>
+        <div class="modal fade" id="topic" tabindex="-1" role="dialog">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title"><i class="fa fa-tags mx-1"></i>Choose Topics</h4>
+                <button type="button" class="close" data-dismiss="modal">
+                  <span>&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p>You need to choose more than one topic.</p>
+                <!-- topic options here -->
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" data-dismiss="modal" 
+                          @click="putStatus(invitation.conference, invitation.inviter, 'accepted')">
+                  Confirm
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
