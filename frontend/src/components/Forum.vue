0946d309f1e22e287c434def1c460c3233586d95
Shi Anqi <Honminden@outlook.com>
2020-06-03 20:31:13 +0800

    added get&post discussions

--- frontend/src/components/Forum.vue
+++ frontend/src/components/Forum.vue
@@ -5,55 +5,41 @@
     <LeftNav :parent="this"/>
     <div class="container col-sm-10" style="margin-top: 15px">
       <InnerNav :parent="this" class="mb-3"/>
-      <div class="accordion" id="accordion">
+      <div v-for="distribution in distributions" :key="distribution.order" class="accordion">
         <div class="card border-light">
-          <button class="btn btn-light card-header text-left" data-toggle="collapse" data-target="#test">
-            test submission
+          <button class="btn btn-light card-header text-left" data-toggle="collapse" 
+                    :data-target="'#discuss'+distribution.title.replace(/[ :]/g, '-').replace()" @click="toggle(distribution)">
+            {{ distribution.title }}
             <i class="fa fa-bars float-right"> </i>
 
           </button>
-          <div id="test" class="collapse" data-parent="#accordion">
+          <div :id="'discuss'+distribution.title.replace(/[ :]/g, '-')" class="collapse">
             <div class="card-body row">
               <div class="col-sm-8">
                 <div class="discuss">
-<!--他人发言-->
-                  <div class="row  mb-4">
-                    <h5 class="col-sm-2 mt-3 text-center">chair</h5>
-                    <div class="col-sm-10">
-                      <div class="bubble-left rounded-lg card border-0" style="width: 350px; background: whitesmoke">
-                        <div class="card-body">
-                          <p class="card-text">
-                            Some quick example text to build on the card title and make up the bulk of the card's content.
-                            Some quick example text to build on the card title and make up the bulk of the card's content.
-                            Some quick example text to build on the card title and make up the bulk of the card's content.
-                          </p>
+                  <div v-for="floor in floors" :key="floor.order">
+                    <small>#{{ floor.order }}</small>
+                    <div class="row mb-4" v-if="user.getUserInfo().username === floor.username">
+                      <div class="col-sm-10">
+                        <div class="bubble-right rounded-lg card float-right border-0" style="width: 350px; background: lightgreen">
+                          <div class="card-body">
+                            <p class="card-text">{{ floor.text }}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <h5 class="col-sm-2 mt-3">ME</h5>
+                    </div>
+                    <div class="row mb-4" v-else>
+                      <h5 class="col-sm-2 mt-3 text-center">{{ floor.username }}</h5>
+                      <div class="col-sm-10">
+                        <div class="bubble-left rounded-lg card border-0" style="width: 350px; background: whitesmoke">
+                          <div class="card-body">
+                            <p class="card-text">{{ floor.text }}</p>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-
-                  <div class="row mb-4">
-                    <h5 class="col-sm-2 mt-3 text-center">PC1</h5>
-                    <div class="col-sm-10">
-                      <div class="bubble-left rounded-lg card border-0" style="width: 350px; background: whitesmoke">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-<!--本人发言-->
-                  <div class="row mb-4">
-                    <div class="col-sm-10">
-                      <div class="bubble-right rounded-lg card float-right border-0" style="width: 350px; background: lightgreen">
-                        <div class="card-body">
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                      </div>
-                    </div>
-                    <h5 class="col-sm-2 mt-3">ME</h5>
-                  </div>
-
                 </div>
               </div>
 
@@ -62,12 +48,12 @@
 
                   <hr>
                 <div class="form-group">
-                  <textarea class="form-control" id="reply" rows="10" v-model='text'></textarea>
+                  <textarea class="form-control" id="reply" rows="10" v-model='reply'></textarea>
                 </div>
 
                 <div class="row mr-3 float-right">
                   <button class="btn btn-danger rounded-left" data-toggle="modal" data-target="#revise">Revise</button>
-                  <button class="btn btn-info  rounded-right">Reply</button>
+                  <button class="btn btn-info  rounded-right" @click.prevent="postDiscussion(distribution)">Reply</button>
 
                   <!--                  revise-->
                   <div class="modal fade" id="revise" tabindex="-1">
@@ -181,12 +167,18 @@
   import Navbar from './Navbar'
   import InnerNav from './InnerNav'
   import User from './User/User'
+  import Alert from './Message/Alert';
   import LeftNav from './LeftNav';
     export default {
         name: "Forum",
       data () {
         return {
           user: new User(),
+          alert: new Alert(),
+          distributions: [],
+          discussions: [],
+          floors: [],
+          reply: '',
           rating: '',
           confidence: '',
           text: '',
@@ -199,7 +191,108 @@
           LeftNav,
           'Navbar': Navbar,
           'InnerNav': InnerNav
+        },
+            methods: {
+        toggle (distribution) {
+          let floors = [];
+          for (let index in Object.keys(this.discussions))
+          {
+            let discussion = this.discussions[index];
+            if ((discussion.conference === distribution.conference) && 
+              (discussion.author === distribution.author) && 
+              (discussion.title === distribution.title))
+            {
+              let floor = {
+                username: discussion.username,
+                order: discussion.order,
+                text: discussion.text
+              };
+              floors.push(floor);
+            }
+          }
+          floors.sort((a, b) => (parseInt(a.order) - parseInt(b.order)));
+          this.floors = floors;
+          this.reply = '';
+        },
+        getDistributions () {
+          this.$axios.get('/distribution', {
+            params: {
+              username: this.user.getUserInfo().username,
+              conference: this.fullName
+            }
+          })
+          .catch(
+            error =>
+            {
+              this.alert.popDanger('fetch distributions error');
+            }
+          )
+          .then(res =>
+          {
+            if(res && res.status === 200)
+            {
+              this.distributions = res.data.distributions;
+            }
+          });
+        },
+        getDiscussions () {
+          this.$axios.get('/discussion', {
+            params: {
+              username: this.user.getUserInfo().username,
+              conference: this.fullName
+            }
+          })
+          .catch(
+            error =>
+            {
+              this.alert.popDanger('fetch discussions error');
+            }
+          )
+          .then(res =>
+          {
+            if(res && res.status === 200)
+            {
+              this.discussions = res.data.discussions;
+            }
+          });
+        },
+        postDiscussion (distribution) {
+          this.$axios.post('/discussion',
+            {
+              conference: this.fullName,
+              author: distribution.author,
+              title: distribution.title,
+              username: this.user.getUserInfo().username,
+              text: this.reply
+            })
+            .catch(
+              error =>
+              {
+                if (error.response.status === 403)
+                {
+                  this.alert.popDanger('discussion submission error');
+                }
+                else
+                {
+                  this.alert.popDanger('submission error');
+                }
+              }
+            )
+            .then(res =>
+            {
+              if(res && res.status === 200)
+              {
+                this.alert.popSuccess('discussion submitted');
+                this.$router.go();
+              }
+            });
         }
+      },
+      mounted () {
+        document.title += ` - ${this.fullName}`;
+        this.getDistributions();
+        this.getDiscussions();
+      }
     }
 </script>
 
